eb9e8173b45741408db48102b8a8f8007256072f
Sam Bessalah <samklr@users.noreply.github.com>
2015-03-10 12:33:27 +0100

    Update UserMining.scala

--- src/main/scala/com/duchessfr/spark/part1.intro/UserMining.scala
+++ src/main/scala/com/duchessfr/spark/part1.intro/UserMining.scala
@@ -1,5 +1,26 @@
 package com.duchessfr.spark.part1.intro
 
-object UserMining {
+object UserMining extends App{
+  
+ val data = "data/reduced-tweets.txt";
+
+  // create spark  configuration and spark context
+  val conf = new SparkConf()
+    .setAppName("HashTagMining")
+    .setMaster("local[*]")
+
+  val sc = new SparkContext(conf)
+
+ // Load the data  and parse it into a Tweet. Look at the Tweet Objetc in the TweetUtils class.
+  val tweets = sparkContext.textFile(data)
+                           .mapPartitions(TweetUtils.parseFromJson(_))
+                           .cache
+
+ 
+  // Find all the tweets by user
+ 
+
+    // Find how many tweets each user has, and find Top 10 people which the most tweets
+  
 
 }
